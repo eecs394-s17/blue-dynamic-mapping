865d0b7d18580a0e526f6f10f0c25aad6356a929
Tony Cao <yzcaoye@163.com>
2017-05-10 21:38:02 -0500

    delete redundant code

--- src/pages/home-page/home-page.ts
+++ src/pages/home-page/home-page.ts
@@ -11,12 +11,11 @@ import { StorageService } from '../../providers/storage-service';
   providers: [StorageService]
 })
 export class HomePage {
+  // use code from https://github.com/driftyco/ionic-preview-app/tree/master/src/pages/slides/basic
   slides = [
     {
       title: "Welcome to the Dynamic Mapping!",
       description: "Dynamic mapping and integration is a form of couples counseling in which couples create a map of their relationship dynamic as a tool to identify, heal and improve problem areas of the relationship. The map includes elements from each person’s family of origin, communication styles, conflict areas and reactions, strength, vulnerabilities and goals. Couples collaboratively and compassionately integrate new awarenesses, tools and healthy new pathways to create a happier and peaceful relationship.",
-      // image: "assets/img/ica-slidebox-img-3.png",
-
     },
 
   ];
