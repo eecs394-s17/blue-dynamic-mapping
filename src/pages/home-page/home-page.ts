97741878a7fbcf5780d38ef16849b439af5c65e0
Akshay Batra <akshaybatra2018@u.northwestern.edu>
2017-05-30 12:04:28 -0500

    Added New Home Page Text

--- src/pages/home-page/home-page.ts
+++ src/pages/home-page/home-page.ts
@@ -19,8 +19,8 @@ export class HomePage {
   slides = [
     {
       title: "Welcome to Dynamic Mapping!",
-      description: "Dynamic mapping and integration is a form of couples counseling in which couples create a map of their relationship dynamic as a tool to identify, heal and improve problem areas of the relationship. The map includes elements from each person’s family of origin, communication styles, conflict areas and reactions, strength, vulnerabilities and goals. Couples collaboratively and compassionately integrate new awarenesses, tools and healthy new pathways to create a happier and peaceful relationship.",
-    },
+      description: `Dynamic mapping and integration couple’s counseling helps couples identify and create balance in the dynamic of their relationship. The dynamic is the pattern of the interaction of thoughts feelings and actions in a relationship. Understanding comes through discovering how information from their past, approach to conflict, communication styles and love languages interact with and effect their partner and the relationship. This app will assist you in your understanding of the dynamic map. You will track your conflict with your partner. this will allow you to better identify patterns as well as what works and doesn’t work. In addition, you will be given prompts that will allow you to address current conflict in a more successful way thus decreasing the time spent in states of hurt, anger and resentment. Following the prompts that you collaboratively help to set, you will have more success navigating conflict areas of the relationship and have more happiness and peace.`,
+    }
   ];
 
   constructor(public navCtrl: NavController, private storageService: StorageService, public authData: AuthData, public _app: App) {
