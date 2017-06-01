b81eca18f2efc3820fb54f5f8edc4ca2a496d893
Stan <stanhuang2017@u.northwestern.edu>
2017-05-31 20:55:41 -0500

    pushing whatever

--- src/pages/home-page/home-page.ts
+++ src/pages/home-page/home-page.ts
@@ -20,7 +20,7 @@ export class HomePage {
   slides = [
     {
       title: "Welcome to Dynamic Mapping!",
-      description: `Dynamic mapping and integration couple’s counseling helps couples identify and create balance in the dynamic of their relationship. The dynamic is the pattern of the interaction of thoughts feelings and actions in a relationship. Understanding comes through discovering how information from their past, approach to conflict, communication styles and love languages interact with and effect their partner and the relationship. This app will assist you in your understanding of the dynamic map. You will track your conflict with your partner. this will allow you to better identify patterns as well as what works and doesn’t work. In addition, you will be given prompts that will allow you to address current conflict in a more successful way thus decreasing the time spent in states of hurt, anger and resentment. Following the prompts that you collaboratively help to set, you will have more success navigating conflict areas of the relationship and have more happiness and peace.`,
+      description: `Dynamic mapping and integration couple’s counseling helps couples identify and create balance in the dynamic of their relationship. This app will assist you in your understanding of the dynamic map. You will track your conflict with your partner. this will allow you to better identify patterns as well as what works and doesn’t work. Following the prompts that you collaboratively help to set, you will have more success navigating conflict areas of the relationship and have more happiness and peace.`,
     }
   ];
 
